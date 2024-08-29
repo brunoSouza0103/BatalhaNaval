92c491bb47d94e47c22761fc517486dadb69d65e
LuizHillebrande <luizhill@edu.unifil.br>
2024-08-28 21:33:07 -0300

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -1,10 +1,15 @@
-def capturar_entrada(jogador):
+def capturar_entrada(jogador, coordenadas_jogadas):
     while True:
         entrada = input(f"{jogador}, insira suas coordenadas de ataque (formato: linha,coluna): ")
         try:
             linha, coluna = map(int, entrada.split(','))
             if 0 <= linha < 10 and 0 <= coluna < 10:
-                return linha, coluna
+                coordenada = (linha, coluna)
+                if coordenada in coordenadas_jogadas:
+                    print("Você já jogou nessa coordenada. Tente outra.")
+                else:
+                    coordenadas_jogadas.add(coordenada)
+                    return linha, coluna
             else:
                 print("Coordenadas fora do intervalo permitido. Tente novamente.")
         except ValueError:
@@ -15,8 +20,9 @@ def exibir_resultado(mensagem):
 
 def main():
     jogador = "Jogador 1"
+    coordenadas_jogadas = set()
     while True:
-        linha, coluna = capturar_entrada(jogador)
+        linha, coluna = capturar_entrada(jogador, coordenadas_jogadas)
         mensagem = f"Coordenadas capturadas: Linha {linha}, Coluna {coluna}"
         exibir_resultado(mensagem)
 
